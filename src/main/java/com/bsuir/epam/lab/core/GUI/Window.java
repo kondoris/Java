b4588cfa0484c9baf7f4c76679bd3a1e5ac7db32
kondoris <62340242+kondoris@users.noreply.github.com>
2020-05-26 05:46:41 +0300

    Create Window.java

---
+++ src/main/java/com/bsuir/epam/lab/core/GUI/Window.java
@@ -0,0 +1,45 @@
+package com.bsuir.epam.lab.core.gui;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Window extends JFrame{
+ 
+	private Human human = new Human();
+	private String str = null;
+	
+	private JButton button1 = new JButton("Ïðèãîòîâèòü åäó");
+	private JButton button2 = new JButton("Íàïèñàòü êîä");
+	
+	private ActionListener action1 = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			e = human.getName().moveFluid(e);
+		}
+	};
+	private ActionListener action2 = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			human.getName().writeCode();
+		}
+	};
+	
+	Window(String name){
+		super(name);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 250);
+		setLocation(400, 400);
+		setVisible(true);
+		this.getContentPane().setLayout(new GridLayout(4, 1, 20, 20));
+		
+		getContentPane().add(button1);
+		getContentPane().add(button2);
+		
+		button1.addActionListener(action1);
+		button2.addActionListener(action2);
+	}
+}
